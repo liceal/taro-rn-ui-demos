ccdc335d68832c7b94c208d88b3c01cad6bd9f0f
shougong <shougong@burgeon.cn>
2023-07-30 23:28:01 +0800

    feat: ui库 prettier eslintfix

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,11 @@
-const { mergeConfig } = require('metro-config')
-const { getMetroConfig } = require('@tarojs/rn-supporter')
-module.exports = mergeConfig({
-  // custom your metro config here
-  // https://facebook.github.io/metro/docs/configuration
-  resolver: {}
-}, getMetroConfig())
+const { mergeConfig } = require("metro-config");
+const { getMetroConfig } = require("@tarojs/rn-supporter");
+
+module.exports = mergeConfig(
+  {
+    // custom your metro config here
+    // https://facebook.github.io/metro/docs/configuration
+    resolver: {},
+  },
+  getMetroConfig(),
+);
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,7 @@
 import { Component, PropsWithChildren } from "react";
 import { View, Text } from "@tarojs/components";
 import "./index.scss";
+import { Button } from "@react-native-material/core";
 
 export default class Index extends Component<PropsWithChildren> {
   componentDidMount() {}
@@ -13,8 +14,9 @@ export default class Index extends Component<PropsWithChildren> {
 
   render() {
     return (
-      <View className='index'>
+      <View className="index">
         <Text>Hello world!111</Text>
+        <Button title="Click Me" onPress={() => alert("🎉🎉🎉")} />
       </View>
     );
   }
